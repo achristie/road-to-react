6758501cb0541d13cfc8b5cc92c989f0e770fac3
Andrew Christie <awchristie86@gmail.com>
2022-12-12 22:31:02 -0500

    pg 197

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useReducer } from "react";
 import { useCallback } from "react";
+import { ReactComponent as Check } from "./check.svg";
+import { useMemo } from "react";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
@@ -19,43 +21,50 @@ function App() {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
-  const handleFetchStories = useCallback(() => {
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFetchStories = useCallback(async () => {
     if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    getAsyncStories(url).then((result) => {
+
+    try {
+      const result = await fetch(url).then((res) => res.json());
+
       dispatchStories({
         type: "STORIES_FETCH_SUCCESS",
         payload: result.hits,
       });
-    });
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
   }, [url]);
 
   useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  const handleRemoveStory = (item) => {
+  const handleRemoveStory = useCallback((item) => {
     dispatchStories({
       type: "REMOVE_STORY",
       payload: item,
     });
-  };
+  }, []);
 
+  console.log("app");
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="submit">Fetch</button>
-      </form>
+      <SearchForm
+        searchTerm={searchTerm}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
       {stories.isLoading ? (
         <h1>Loading!</h1>
       ) : (
-        <ul>{stories.data.map((s) => Stories(s, handleRemoveStory))}</ul>
+        <Stories list={stories.data} handleRemove={handleRemoveStory} />
       )}
     </div>
   );
@@ -63,31 +72,44 @@ function App() {
 
 export default App;
 
+const SearchForm = ({ searchTerm, handleSubmit, handleChange }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <button disabled={!searchTerm} type="submit">
+        Fetch
+      </button>
+      <Check height="18px" width="18px" fill="green" stroke="green" />
+    </form>
+  );
+};
+
 const useStorageState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
 
   useEffect(() => {
+    console.log("str state");
     localStorage.setItem(key, value);
   }, [value]);
 
   return [value, setValue];
 };
 
-function Stories(item, handleRemove) {
-  return (
-    <li key={item.objectID}>
-      {item.title}
-      <button type="button" onClick={() => handleRemove(item)}>
-        Remove
-      </button>
-    </li>
-  );
-}
-
-async function getAsyncStories(url) {
-  let resp = await fetch(url).then((res) => res.json());
-  return resp;
-}
+const Stories = React.memo(
+  ({ list, handleRemove }) =>
+    console.log("list") || (
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            {item.title}
+            <button type="button" onClick={() => handleRemove(item)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    )
+);
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
